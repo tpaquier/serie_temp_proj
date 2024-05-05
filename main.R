afdab7f11699ca37d88c1ce7fcf2144fb30ade1a
grn-matthieu <grn.matthieu@yahoo.com>
2024-05-05 13:59:31 +0200

    création projet

---
+++ main.R
@@ -0,0 +1,20 @@
+#==============================================================================#
+##### Packages ####
+#==============================================================================#
+
+library(zoo)
+library(tseries)
+library(fUnitRoots)
+library(purrr)
+
+
+#==============================================================================#
+##### Import + Traitement ####
+#==============================================================================#
+
+data <- read.csv("C:/Users/grnma/OneDrive/Bureau/ENSAE/2A/S2/Séries temp/Projet Michelin/serie_010767848_01052024/tmpZipSerieCsv3541213619101186198/valeurs_mensuelles.csv", header=FALSE, sep=";")
+str(data)
+
+data <- data[5:nrow(data), 0:2]
+colnames(data) <- c('Date', 'Valeur')
+data$Date <- as.Date(paste(data$Date, "-01", sep = ""), format = "%Y-%m-%d")
